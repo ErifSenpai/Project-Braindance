7c36739670b318300cce539c68a38972cd4b3e75
DommoRGB <dominique.bitetti@gmail.com>
2023-04-03 11:37:36 +0200

    Passaggi coding

---
+++ code/gameplay.js
@@ -0,0 +1,14 @@
+//Sessioni
+//Array di domande
+//Array di risposte giuste
+//Array di risposte sbagliate
+//HashMap che lega domanda a risposta giusta
+//Array di argomenti delle domande
+//Sub-array di domande per ogni argomento
+//Sub-array di risposte giuste e sbagliate legato alla domanda e/o alla categoria
+//Scelta delle domande da una categoria scelta dall'utente
+//Domande pescate casualmente da array di quel tipo di domande
+//Scelta casuale della domanda viene salvata in un array specifico con di seguito altri 2 array con le risposte relative alla domanda
+//Confronto di quello che viene cliccato dall'utente con quale risposta sia
+    //Se giusta punteggio +10
+    //Se sbgaliata punteggio +0
