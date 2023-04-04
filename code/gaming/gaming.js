afab767161d2d7e1c87ff3a1a1746e13b7a664c2
Dommo <dominique.bitetti@gmail.com>
2023-04-04 17:27:44 +0200

    Adding css n a lil code

---
+++ code/gaming/gaming.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', attivaEventi);
+
+var gameType;
+
+function attivaEventi()
+{
+    document.getElementById("sport").addEventListener("click", sportType);
+    document.getElementById("musica").addEventListener("click", musicType);
+    document.getElementById("games").addEventListener("click", videogameType);
+    document.getElementById("cinema").addEventListener("click", cinemaType);
+}
+
+function musicType()
+{
+    gameType = "musica";
+    console.log(gameType);
+}
+
+function sportType()
+{
+    gameType = "sport";
+    console.log(gameType);
+}
+
+function cinemaType()
+{
+    gameType = "cinema";
+    console.log(gameType);
+}
+
+function videogameType()
+{
+    gameType = "videogiochi";
+    console.log(gameType);
+}
+
+
+
